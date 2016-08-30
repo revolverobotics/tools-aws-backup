1f4226f9bb1fb5bfbeb69c4a93fdb7493c4e570b
Timothy Huang <heisian@Administrators-MacBook-Pro.local>
2016-08-30 12:12:10 -0700

    update

--- Backup-AWS-Instances.sh
+++ Backup-AWS-Instances.sh
@@ -1,6 +1,6 @@
 echo "Enumerating all EC2 instances for backup..."
 
-EC2_INSTANCES="$(aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId,State.Name,Tags[?Key=='Name'].Value | [0], InstanceType]" --output text)"
+EC2_INSTANCES="$(aws ec2 describe-instances --query "Reservations[].Instances[].[InstanceId,State.Name,Tags[?Key=='Name'].Value | [0], BlockDeviceMappings[], InstanceType]" --output text)"
 
 IFS=$'\n' read -rd '' -a EC2_INSTANCES_ARRAY <<< "${EC2_INSTANCES}"
 
